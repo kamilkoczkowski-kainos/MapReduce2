4d0f472a2fb644f032d68dd4f484cfe8a8ac22c9
Kamil Koczkowski <kamilk@kainos.com>
2018-03-19 12:03:26 +0100

    Changes in reducer, added %

--- src/main/java/io/datamass/MapWordCount.java
+++ src/main/java/io/datamass/MapWordCount.java
@@ -6,35 +6,19 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-public class MapWordCount extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class MapWordCount extends Mapper<LongWritable, Text, IntWritable, Text> {
 
     public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException
     {
         String line = value.toString();
-        String[] words=line.split("\t");
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int i=0; i<words.length; i++)
-        {
-            String word = i+"-"+words[i];
-            Text outputKey = new Text(word.toUpperCase().trim());
-            IntWritable outputValue = new IntWritable(1);
-            con.write(outputKey, outputValue);
+        String[] words=line.split("\\t");
+        for (int col = 0; col < words.length; col++ ) {
 
-            if (i>=list.size()) {
-                list.add(1);
-            } else {
-                int temp = list.get(i);
-                list.set(i,temp+1);
-            }
-        }
-
-        for (int z=0; z<list.size(); z++) {
-            Text outputKey = new Text(z+"-");
-            IntWritable outputValue = new IntWritable(list.get(z));
+            String word = words[col];
+            Text outputValue = new Text(word.trim());
+            IntWritable outputKey = new IntWritable(col);
             con.write(outputKey, outputValue);
         }
     }
-
 }
--- src/main/java/io/datamass/ReducerWordCount.java
+++ src/main/java/io/datamass/ReducerWordCount.java
@@ -1,21 +1,34 @@
+
 package io.datamass;
 
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
-
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-
-public class ReducerWordCount extends Reducer<Text, IntWritable, Text, IntWritable>
+public class ReducerWordCount extends Reducer<IntWritable, Text, IntWritable, Text>
 {
-    public void reduce(Text word, Iterable<IntWritable> values, Context con) throws IOException, InterruptedException
+    public void reduce(IntWritable column, Iterable<Text> values, Context con) throws IOException, InterruptedException
     {
-        int sum = 0;
-        for(IntWritable value : values)
+        Map<Text, Integer> myMap = new HashMap<Text, Integer>();
+        int countInt = 0;
+        for (Text word : values)
         {
-            sum += value.get();
+            int count = myMap.containsKey(word) ? myMap.get(word) : 0;
+            myMap.put(new Text(word), count + 1);
+            countInt++;
         }
-        con.write(word, new IntWritable(sum));
+        for (Map.Entry<Text, Integer> entry : myMap.entrySet()) {
+            String out = String.format("word: %s  how many: %s [%.2f %%]",
+                    entry.getKey().toString(),
+                    entry.getValue().toString(),
+                    entry.getValue() * 100F / countInt);
+
+
+            con.write(column, new Text(out));
+        }
+
     }
 }
